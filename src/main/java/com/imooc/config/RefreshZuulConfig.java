ce63df5546ae7bea8e3d65f09801c25ce130fffe
mixluo <luoqiang92@126.com>
2018-08-18 22:48:03 +0800

    在配置中心增加网关服务的配置文件

---
+++ src/main/java/com/imooc/config/RefreshZuulConfig.java
@@ -0,0 +1,16 @@
+package com.imooc.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RefreshZuulConfig {
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
+    }
+}
